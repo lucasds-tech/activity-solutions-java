a4bfeef3b1856916a9339bc4be590baecace77b4
Lucas da Silva <lucas.dasilva09@hotmail.com>
2025-01-17 17:46:59 -0300

    Update to add and remove products

--- program_product/src/application/Program.java
+++ program_product/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -23,7 +24,24 @@ public class Program {
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product); //toString
+		System.out.println();
+		System.out.println("Product data: " + product); //toString
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity); // atualização da quantidade, dentro do objeto 'product'
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		
 		sc.close();
 	}
