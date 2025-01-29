fae9734dd43903349b71e52ad23f8d3c249a5c5f
Lucas da Silva <lucas.dasilva09@hotmail.com>
2025-01-29 11:21:45 -0300

    Inserting into the database

--- jpa-hibernate/jpamaven/src/main/java/aplicacao/Programa.java
+++ jpa-hibernate/jpamaven/src/main/java/aplicacao/Programa.java
@@ -1,18 +1,33 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Daniela Gomes", "daniela@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Diogenes Azevedo", "diogenes@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-
+		// PESSOAS REGISTRADAS
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Daniela Gomes", "daniela@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Diogenes Azevedo", "diogenes@gmail.com");
+		
+		
+		// CONEXÃO COM BANCO DE DADOS
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		// IMPLEMENTANDO NO BANCO DE DADOS
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
 	}
 
 }
